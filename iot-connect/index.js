985bdfebdcbcf68ec5324034eedf6b40c33b04a6
baievv <baev.dav@gmail.com>
2023-08-07 20:41:41 +0300

    fix: bugs with async (lock operation)

--- iot-connect/index.js
+++ iot-connect/index.js
@@ -1,27 +1,45 @@
 import { requestLockStatus, openLock, closeLock } from "./lock-server.js";
 
-export default async function execCommand(apart, device, command) {
-	console.log("inside execCommand");
-	let result;
-	const lockStatus = await requestLockStatus();
-	console.log("Lock state now is -", lockStatus.state);
-	// if (apart === "batumi" && device === "lock" && lockStatus.state==0) {
-	if (lockStatus.state == 0 && command === "open") {
-		let res = await openLock();
-		console.log("Result of opening is -", res);
-		let lockState = await requestLockStatus();
-		console.log("Lock state now is", lockState.state);
-		if (lockState.state == 1) {
-			result = { status: "Lock was opened" };
-		} else result = { status: "Have some problems" };
-	} else if (lockStatus.state == 1 && command === "close") {
-		let res = await closeLock();
-		console.log("Result of close is -", res);
-		let lockState = await requestLockStatus();
-		console.log("Lock state now is", lockState.state);
-		if (lockState.state == 0) {
-			result = { status: "Lock was closed" };
-		} else result = { status: "Have some problems" };
-	} else result = { status: "Unknown command" };
+const lockStatus = async () => {
+	let result = await requestLockStatus();
 	return result;
+};
+
+const lockOpen = async () => {
+	let result = await openLock();
+	return result;
+};
+
+const lockClose = async () => {
+	let result = await closeLock();
+	return result;
+};
+
+async function sleep(ms) {
+	console.log("sleeping");
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export default async function execCommand(apart, device, command) {
+	let result;
+	const status = await lockStatus();
+	console.log("Lock state now is -", status.state);
+	// if (apart === "batumi" && device === "lock" && lockStatus.state==0) {
+	if (status.state == 0 && command === "switch") {
+		await sleep(500);
+		const res = await lockOpen();
+		console.log("Result of opening is -", res);
+		if (res.errcode === 0) {
+			result = { status: "opened" };
+		} else result = { status: "problems" };
+	};
+	if (status.state == 1 && command === "switch") {
+		sleep(500);
+		const res = await lockClose();
+		if (res.errcode === 0) {
+			result = { status: "closed" };
+		} else result = { status: "problems" };
+	};
+	console.log('Result function is -',result);
+ return result;
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,20 @@
 //routing main
 import execCommand from "../iot-connect/index.js";
 
+async function useCommand(aparts, device, command) {
+	let status = await execCommand(aparts, device, command);
+	console.log("Status is -", status);
+	return status;
+}
+
 export default function (app) {
 	app.get("/dashboard/*/*/*", (req, res) => {
-		console.log("Try to use /dashboard/");
 		const aparts = req.params[0];
 		const device = req.params[1];
 		const command = req.params[2];
 
 		console.log("Aparts is ", aparts, " device is ", device, " command is ", command);
-		let status = execCommand(aparts, device, command);
-		console.log('Status is -', status);
-		res.json(status);
+		res.json(useCommand(aparts, device, command));
+		// res.json(status);
 	});
 }
